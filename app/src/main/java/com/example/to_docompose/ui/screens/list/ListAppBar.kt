bda9ef4a7fa6d9c8788dc95c03336b6cfabc6016
TiliaSNogueira <tiliasalomao@gmail.com>
2023-07-19 23:08:52 -0300

    including icons for app bar actions (delete, filter, search)

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,27 +1,61 @@
 package com.example.to_docompose.ui.screens.list
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.R
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.ui.theme.LARGE_PADDING
+import com.example.to_docompose.ui.theme.SMALL_PADDING
+import com.example.to_docompose.ui.theme.Typography
 import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.example.to_docompose.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {}
+    )
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
     TopAppBar(
         title = {
             Text(text = "Tasks", color = MaterialTheme.topAppBarContentColor)
+        },
+        actions = {
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked,
+                onDeleteClicked = onDeleteClicked
+            )
         },
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.topAppBarBackgroundColor
@@ -30,8 +64,184 @@ fun DefaultListAppBar() {
 }
 
 @Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+    SortAction(onSortClicked = onSortClicked)
+    DeleteAllAction(onDeleteClicked = onDeleteClicked)
+}
+
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit
+) {
+    IconButton(
+        onClick = { onSearchClicked() }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(id = R.string.search_tasks),
+            tint = MaterialTheme.topAppBarContentColor
+        )
+    }
+
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit
+) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(id = R.string.sort_action),
+            tint = MaterialTheme.topAppBarContentColor
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = Priority.LOW.name,
+                        style = Typography.titleMedium,
+                        modifier = Modifier.padding(start = SMALL_PADDING)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_circle),
+                        contentDescription = Priority.LOW.name,
+                        //tint = colorResource(id = R.color.green)
+                        tint = Priority.LOW.color
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                }
+            )
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = Priority.MEDIUM.name,
+                        style = Typography.titleMedium,
+                        modifier = Modifier.padding(start = SMALL_PADDING)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_circle),
+                        contentDescription = Priority.MEDIUM.name,
+                        // tint = colorResource(id = R.color.yellow)
+                        tint = Priority.MEDIUM.color
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                }
+            )
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = Priority.HIGH.name,
+                        style = Typography.titleMedium,
+                        modifier = Modifier.padding(start = SMALL_PADDING)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_circle),
+                        contentDescription = Priority.HIGH.name,
+                        //tint = colorResource(id = R.color.red)
+                        tint = Priority.HIGH.color
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            )
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = Priority.NONE.name,
+                        style = Typography.titleMedium,
+                        modifier = Modifier.padding(start = SMALL_PADDING)
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_circle),
+                        contentDescription = Priority.NONE.name,
+                        //tint = colorResource(id = R.color.white)
+                        tint = Priority.NONE.color
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun DeleteAllAction(
+    onDeleteClicked: () -> Unit
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(id = R.drawable.vertical_menu),
+            contentDescription = stringResource(id = R.string.delete_all_action),
+            tint = MaterialTheme.topAppBarContentColor
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = stringResource(
+                            id = R.string.delete_all_action
+                        ),
+                        style = Typography.titleMedium,
+                        modifier = Modifier.padding(start = LARGE_PADDING)
+                    )
+                },
+                onClick = {
+                    expanded = false
+                    onDeleteClicked()
+                }
+            )
+        }
+    }
+}
+
+@Composable
 @Preview
 private fun DefaultListAppBarPreview() {
-    DefaultListAppBar()
+    DefaultListAppBar(onSearchClicked = {}, onSortClicked = {}, onDeleteClicked = {})
 }
 
